1422151093369b94bcfc52ef23f674ad5dc42915
Ali Ansari <badnan541@gmail.com>
2026-08-02 19:43:30 +0100

    Add weather condition emoji mapping

--- main.py
+++ main.py
@@ -119,14 +119,42 @@ class WeatherApp(QWidget):
     def display_error(self, message):
         self.temperature_label.setStyleSheet("font-size: 40px;")
         self.temperature_label.setText(message)
+        self.emoji_label.clear()
+        self.description_label.clear()
 
     def display_weather(self, data):
         self.temperature_label.setStyleSheet("font-size: 80px;")
         temperature = round(data["main"]["temp"] - 273.15) # kelvin --> celsius
+        weather_id = data["weather"][0]["id"]
         weather_description = data["weather"][0]["description"]
-
         self.temperature_label.setText(f"{temperature}°C")
+        self.emoji_label.setText(self.get_weather_emoji(weather_id))
         self.description_label.setText(weather_description)
+
+    @staticmethod
+    def get_weather_emoji(weather_id):
+        if 200 <= weather_id <= 232:
+            return "⛈️"
+        elif 300 <= weather_id <= 321:
+            return "☁️"
+        elif 500 <= weather_id <= 531:
+            return "🌧️"
+        elif 600 <= weather_id <= 622:
+            return "❄️"
+        elif 701 <= weather_id <= 741:
+            return "💨"
+        elif weather_id == 762:
+            return "🌋"
+        elif weather_id == 771:
+            return "🍃"
+        elif weather_id == 781:
+            return "🌪️"
+        elif weather_id == 800:
+            return "☀️"
+        elif 801 <= weather_id <= 804:
+            return "☁️"
+        else:
+            return ""
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
